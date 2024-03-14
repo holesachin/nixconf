df8cba63432bd57a20df69a8396db75508fe1735
holesachin <holesachin07@gmail.com>
2024-03-14 22:39:56 +0530

    home-manager added

--- flake.nix
+++ flake.nix
@@ -4,16 +4,29 @@
   inputs = {
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let 
+    system = "x86_64-linux";
     lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
+    # system
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+
+    # home-manager
+    homeConfigurations = {
+       sachin = home-manager.lib.homeManagerConfiguration {
+         inherit pkgs;
+         modules = [ ./home.nix ];
       };
     };
   };
